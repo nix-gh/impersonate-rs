59df6998dabdad60bd154bbf71bee8722b41f30e
Nix <nicolas.gervail@gmail.com>
2026-07-28 22:12:36 +0200

    detect if libcurl-impersonate is present in custum or default path

--- build.rs
+++ build.rs
@@ -2,14 +2,16 @@ fn main() {
     // Only link if not mocking
     #[cfg(not(feature = "mock"))]
     {
-        println!("cargo:rustc-link-search=/usr/local/lib");
-        println!("cargo:rustc-link-lib=curl-impersonate-chrome");
+        println!("cargo:rerun-if-env-changed=LIBCURL_IMPERSONATE_DIR");
 
-        // We might also need to tell curl-sys to NOT link against system curl if possible,
-        // or just ensure our symbols are loaded.
-        // curl-sys usually links "curl". If we link "curl-impersonate-chrome" too, it might work
-        // if the symbols don't conflict or if we prefer the latter.
-        // However, curl-impersonate usually REPLACES curl.
+        let library_directory = build_support::find_library()
+            .expect("libcurl-impersonate-chrome was not found");
+
+        println!(
+            "cargo:rustc-link-search=native={}",
+            library_directory.display()
+        );
+        println!("cargo:rustc-link-lib=curl-impersonate-chrome");
 
         println!(
             "cargo:warning=Expecting libcurl-impersonate v{}",
--- crates/build-support/src/lib.rs
+++ crates/build-support/src/lib.rs
@@ -1,1 +1,33 @@
+use std::path::{Path, PathBuf};
+
 pub const LIBCURL_IMPERSONATE_VERSION: &str = "1.5.6";
+
+pub fn find_library() -> Option<PathBuf> {
+    find_from_environment()
+        .or_else(find_system_library)
+}
+
+fn find_from_environment() -> Option<PathBuf> {
+    let directory = std::env::var_os("LIBCURL_IMPERSONATE_DIR")?;
+    let path = PathBuf::from(directory);
+
+    if contains_library(&path) {
+        Some(path)
+    } else {
+        None
+    }
+}
+
+fn find_system_library() -> Option<PathBuf> {
+    let path: PathBuf = PathBuf::from("/usr/local/lib");
+
+    if contains_library(&path) {
+        Some(path)
+    } else {
+        None
+    }
+}
+
+fn contains_library(directory: &Path) -> bool {
+    directory.join("libcurl-impersonate-chrome.so").exists()
+}
